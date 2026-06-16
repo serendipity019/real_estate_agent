d89892ac1f30cfe79ec0df33c1a9837ee8fea49c
serendipity019 <navagio09@gmail.com>
2026-06-16 22:41:07 +0300

    feat: function get_session added.

--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -31,3 +31,7 @@ def init_db(session: Session) -> None:
             is_superuser=True,
         )
         user = crud.create_user(session=session, user_create=user_in)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
